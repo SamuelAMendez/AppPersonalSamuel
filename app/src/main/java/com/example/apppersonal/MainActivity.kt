3b46687cd3ec08abf8f857fc5e4aed1620978f74
SamuelAMendez <samuel_07argueta@yahoo.es>
2020-10-02 23:29:50 -0600

    Initial commit

---
+++ app/src/main/java/com/example/apppersonal/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.apppersonal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*;
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        boton.setOnClickListener {
+            texto.text = "Hola como estas"
+            println(texto)
+            }
+        }
+}
